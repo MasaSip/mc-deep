dec183219791e9af4d76a05fd5b9713778545645
ninpnin <vaino.yrjanainen@aalto.fi>
2019-05-19 14:43:33 +0300

    Implement word embedding in keras model

--- keras-model/k-model.py
+++ keras-model/k-model.py
@@ -6,43 +6,21 @@ from keras.layers import Dropout
 from keras.layers import LSTM
 from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
-# load ascii text and covert to lowercase
-'''
-filename = "wonderland.txt"
-raw_text = open(filename).read()
-raw_text = raw_text.lower()
-# create mapping of unique chars to integers
-chars = sorted(list(set(raw_text)))
-char_to_int = dict((c, i) for i, c in enumerate(chars))
-# summarize the loaded data
-n_chars = len(raw_text)
-n_vocab = len(chars)
-print "Total Characters: ", n_chars
-print "Total Vocab: ", n_vocab
-# prepare the dataset of input to output pairs encoded as integers
-seq_length = 100
-dataX = []
-dataY = []
-for i in range(0, n_chars - seq_length, 1):
-	seq_in = raw_text[i:i + seq_length]
-	seq_out = raw_text[i + seq_length]
-	dataX.append([char_to_int[char] for char in seq_in])
-	dataY.append(char_to_int[seq_out])
-n_patterns = len(dataX)
-print "Total Patterns: ", n_patterns
-# reshape X to be [samples, time steps, features]
-X = np.reshape(dataX, (n_patterns, seq_length, 1))
-# normalize
-X = X / float(n_vocab)
-# one hot encode the output variable
-y = np_utils.to_categorical(dataY)
-
-'''
 
 
 X = np.load("keras-model/X.npy")
 Y = np.load("keras-model/Y.npy")
 y = np_utils.to_categorical(Y)
+
+e_beginning = np.load("data/embeddings/embedding-beginning.npy")
+e_end = np.load("data/embeddings/embedding-end.npy")
+embedding_matrix = np.concatenate((e_beginning, e_end), axis=1)
+vocab_size = len(embedding_matrix)
+
+print(embedding_matrix.shape, vocab_size)
+
+e = Embedding(vocab_size, 128, weights=[embedding_matrix], input_length=20, trainable=False)
+
 
 print(X.shape,Y.shape)
 # define the LSTM model
