8b51c713073ebd9c820894b9d8f91a5727517613
ninpnin <vaino.yrjanainen@aalto.fi>
2019-05-20 00:33:50 +0300

    Refactor and add comments

--- keras-model/k-model.py
+++ keras-model/k-model.py
@@ -9,7 +9,9 @@ from keras.utils import np_utils
 import keras
 from math import log
 
-prefix = ""#'../'
+prefix = ""
+
+# Toy data for quick testing
 toydata = False
 if toydata:
 	X = np.load(prefix + "keras-model/X_0.npy")[:500,:]
@@ -18,7 +20,6 @@ else:
 	X = np.load(prefix + "keras-model/X.npy")[:,:,0]
 	Y = np.load(prefix + "keras-model/Y.npy")
 
-#log(0)
 print("To categorical")
 #y = np_utils.to_categorical(Y)
 print("done")
@@ -26,15 +27,19 @@ print("done")
 
 print(Y)
 
-linebreak_vec = np.zeros((23991,1))
-linebreak_vec[127] = 1.0
+
+# ABOUT THE EMBEDDINGS
+# One embedding is calculated based on how similar the endings 
+# of words are
+# One embedding is calculated based on how similar the beginnings
+# of words are
+# Here the two are combined into one complete embedding
 
 e_beginning = np.load(prefix + "data/embeddings/embedding-beginning.npy")
-e_end = np.load(prefix + "data/embeddings/embedding-end.npy")[:,:63]
+e_end = np.load(prefix + "data/embeddings/embedding-end.npy")[:,:64]
 
 embedding_matrix = np.concatenate((e_beginning, e_end), axis=1)
-embedding_matrix = np.concatenate((embedding_matrix, np.zeros((1,127))), axis=0)
-embedding_matrix = np.concatenate((embedding_matrix, linebreak_vec), axis=1)
+embedding_matrix = np.concatenate((embedding_matrix, np.random.rand(1,128)), axis=0)
 vocab_size = len(embedding_matrix)
 
 print(embedding_matrix.shape, vocab_size)
@@ -42,7 +47,6 @@ print(embedding_matrix.shape, vocab_size)
 e = Embedding(vocab_size, 128, weights=[embedding_matrix], input_length=20, trainable=False)
 
 
-print(X.shape,Y.shape)
 # define the LSTM model
 model = Sequential()
 model.add(e)
@@ -50,24 +54,24 @@ model.add(LSTM(256))
 model.add(Dropout(0.1))
 model.add(Dense(128, activation='linear'))
 
-print(23991)
 d = Dense(23991, activation='softmax', trainable=False)
 model.add(d)
 
-print(d.get_config())
-print(d.get_weights())
+# Prior probability of each word
 U_log = np.load(prefix + "keras-model/U_log.npy")
 
-d.set_weights([embedding_matrix.T, U_log * 0.5])
-# log-loss
-#filename = "data/keras-checkpoints/weights-improvement-02-8.0520.hdf5"
-#model.load_weights(filename)
+# Import weights from earlier training
+filename = "data/keras-checkpoints/weights-improvement-07-5.1608.hdf5"
+model.load_weights(filename)
+
+# Set weights for different classes
+d.set_weights([embedding_matrix.T, U_log * 1.0])
+
 
 model.compile(loss='categorical_crossentropy', optimizer='adam')
-# define the checkpoint
 filepath = prefix + "data/keras-checkpoints/weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
 checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
 callbacks_list = [checkpoint]
-# fit the model
+
 model.fit(X, Y, epochs=20, batch_size=128, callbacks=callbacks_list)
 
