bde2d1fd49238b9453552d9b8e020a52f7669544
Matti Sippola <matti.sippola@newsec.fi>
2019-05-20 00:26:49 +0300

    Delete useless files

---
+++ data/keras-checkpoints/README.md
@@ -0,0 +1,1 @@
+# Save Keras checkpoints here
--- model/main.py
+++
@@ -1,7 +0,0 @@
-from importlib import reload
-
-import main
-reload(main)
-main.params[0]['skip_training'] = True
-main.McDiippi('cpu',**main.params[0])
-
